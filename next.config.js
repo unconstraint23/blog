d66495440262f50df3a39853886f4fc4310e9bf9
zrj <rj527667654@163.com>
2022-08-03 00:52:56 +0800

    第一步：初始化

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
+
+const path = require("path");
+
+const resolve = dir => path.resolve(__dirname, dir);
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
---
+++ react-app-env.d.ts
@@ -0,0 +1,4 @@
+declare module '*.js'
+declare module 'redux-thunk'
+declare module '*.json'
+declare module 'three';
